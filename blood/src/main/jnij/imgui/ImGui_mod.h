8c800eb0a4e0864bd77000b91e9c088bb8a0de73
Coco martin <150522458+joshuaAposto@users.noreply.github.com>
2026-04-06 01:43:00 +0000

    Hi

--- blood/src/main/jnij/imgui/ImGui_mod.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void displayKeyboard(bool pShow);
--- blood/src/main/jnij/include/hpp/imgui.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "../../imgui/imgui.h"
-#include "../../imgui/imstb_truetype.h"
-#include "../../imgui/imstb_textedit.h"
-#include "../../imgui/imstb_rectpack.h"
-#include "../../imgui/backends/imgui_impl_android.h"
-#include "../../imgui/backends/imgui_impl_opengl3.h"
