4b2872986fb927faecc300bc38403e5e82f6ab36
Coco martin <150522458+joshuaAposto@users.noreply.github.com>
2026-04-09 05:55:44 +0000

    Hi

--- bs/src/main/obj/local/arm64-v8a/objs/BSglobal/xhook/xh_version.o.d
+++
@@ -1,4 +0,0 @@
-src/main/obj/local/arm64-v8a/objs/BSglobal/xhook/xh_version.o: \
-  src/main/jni/xhook/xh_version.c src/main/jni/xhook/xh_version.h
-
-src/main/jni/xhook/xh_version.h:
--- bs/src/main/obj/local/arm64-v8a/objs/Rary/xhook/xhook.o.d
+++
@@ -1,7 +0,0 @@
-src/main/obj/local/arm64-v8a/objs/rary/xhook/xhook.o: \
-  src/main/jni/xhook/xhook.c src/main/jni/xhook/xh_core.h \
-  src/main/jni/xhook/xhook.h
-
-src/main/jni/xhook/xh_core.h:
-
-src/main/jni/xhook/xhook.h:
